d7831610faa1e0569e1b0eaae39a11de08f633e7
Stephen Edwards <sventi555@gmail.com>
2019-01-04 12:05:25 -0500

    added str for graphic

--- graphics/models.py
+++ graphics/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 class Graphic(models.Model):
     title = models.CharField(max_length=140, unique=True)
-    link = models.TextField()
+    iframe = models.TextField()
     description = models.TextField()
+
+    def __str__(self):
+            return self.title
+
 
