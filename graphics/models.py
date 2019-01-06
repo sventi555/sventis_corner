cfcc78141c96cc7b1a50f838889572731bbdf3b2
Stephen Edwards <sventi555@gmail.com>
2019-01-05 21:18:46 -0500

    finally mending the database mess

--- graphics/models.py
+++ graphics/models.py
@@ -5,6 +5,7 @@ class Graphic(models.Model):
     title = models.CharField(max_length=140, unique=True)
     iframe = models.TextField()
     description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
             return self.title
